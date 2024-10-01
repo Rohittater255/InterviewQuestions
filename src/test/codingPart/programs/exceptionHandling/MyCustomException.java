9963745096fb5f0f17fe4b119cce1e6641873560
rohit <rohittater1994@gmail.com>
2024-10-02 00:27:48 +0530

    UPdates

---
+++ src/test/codingPart/programs/exceptionHandling/MyCustomException.java
@@ -0,0 +1,18 @@
+package test.codingPart.programs.exceptionHandling;
+
+public class MyCustomException extends Exception {
+
+    // Constructor without message
+    public MyCustomException() {
+    }
+
+    // Constructor that accepts a message
+    public MyCustomException(String message) {
+        super(message);
+    }
+
+    // Constructor that accepts a message and a cause
+    public MyCustomException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/test/codingPart/programs/exceptionHandling/TryCatch.java
@@ -0,0 +1,44 @@
+package test.codingPart.programs.exceptionHandling;
+
+import org.testng.annotations.Test;
+
+public class TryCatch {
+
+    @Test
+    public void tryWithoutCatch() {
+        try {
+            System.out.println("Generic Try Block ");
+            throw new MyCustomException("Generic Exception Message");
+        }
+        catch (MyCustomException e){
+            System.out.println("Generic Catch Block ");
+            System.out.println("Exception "+e);
+        }finally {
+            System.out.println("Generic Finally Block");
+        }
+    }
+
+    @Test
+    public void tryWithMultipleCatch() {
+        try {
+            System.out.println("Generic Try Block ");
+        } catch (NullPointerException ne) {
+            ne.getMessage();
+        } catch (Exception e) {
+            e.getMessage();
+        } finally {
+            System.out.println("Generic Finally Block");
+        }
+    }
+
+    @Test
+    public void systemExit() {
+        try {
+            System.exit(0);//This will be executed
+        } catch (Exception e) {
+            System.out.println("The Exception is: " + e);//This will not be executed
+        } finally {
+            System.exit(1);//This will not be executed
+        }
+    }
+}
